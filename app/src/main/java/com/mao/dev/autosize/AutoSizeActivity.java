ce70fef5737151e74d0f73fdcd08de47633d6700
xrealm <mpging.12315@gmail.com>
2016-10-23 21:10:12 +0800

    test resize

--- app/src/main/java/com/mao/dev/autosize/AutoSizeActivity.java
+++ app/src/main/java/com/mao/dev/autosize/AutoSizeActivity.java
@@ -1,8 +1,14 @@
 package com.mao.dev.autosize;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import com.mao.dev.R;
 
@@ -16,5 +22,32 @@ public class AutoSizeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autosize);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+        final View editText = findViewById(R.id.tv_autosize);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWindow().getDecorView().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Rect rect = new Rect();
+                        getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+                        Log.d("mpg", "bottom=" + rect.bottom);
+                    }
+                }, 1000);
+            }
+        });
+
+        findViewById(R.id.rl_content).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(editText);
+            }
+        });
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
