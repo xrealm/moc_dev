6faf7d5a6d197f23a18726caf33ea3517d7bfde0
xrealm <mpging.12315@gmail.com>
2016-11-01 22:42:09 +0800

    camera

---
+++ app/src/main/java/com/mao/dev/filter/CameraActivity.java
@@ -0,0 +1,17 @@
+package com.mao.dev.filter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mao on 2016/11/1.
+ */
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
