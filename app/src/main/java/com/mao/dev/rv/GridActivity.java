b2095f38d656f76c1de600e091333b3306a16ae9
xrealm <mpging.12315@gmail.com>
2016-11-14 23:45:09 +0800

    import recyclerview

---
+++ app/src/main/java/com/mao/dev/rv/GridActivity.java
@@ -0,0 +1,20 @@
+package com.mao.dev.rv;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mao.dev.R;
+
+/**
+ * Created by Mao on 2016/11/14.
+ */
+
+public class GridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rvgrid);
+    }
+}
