2bf5507aef4c6b0eacdb438f6adc95a2e7bcde47
xrealm <mpging.12315@gmail.com>
2016-12-24 18:43:36 +0800

    qr

--- app/src/main/java/com/mao/dev/ui/qr/QRActivity.java
+++ app/src/main/java/com/mao/dev/ui/qr/QRActivity.java
@@ -4,9 +4,9 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
-import com.mao.dev.AppKit;
 import com.mao.dev.R;
 
 /**
@@ -15,21 +15,26 @@ import com.mao.dev.R;
 
 public class QRActivity extends AppCompatActivity {
 
+    private ImageView mImageView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
 
-        ImageView imageView = (ImageView) findViewById(R.id.iv_qr);
+        mImageView = (ImageView) findViewById(R.id.iv_qr);
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = new QRCodeEncoder.Builder().width(800)
+                        .height(800)
+                        .paddingPx(0)
+                        .marginPt(1)
+                        .build()
+                        .encode("http://www.immomo.com");
+                mImageView.setImageBitmap(bitmap);
+            }
+        });
 
-        Bitmap bitmap = new QRCodeEncoder.Builder().width(800)
-                .height(800)
-                .paddingPx(0)
-                .marginPt(3)
-                .build()
-                .encode("http://www.immomo.com");
-        imageView.setImageBitmap(bitmap);
-
-        AppKit.getHostIp();
     }
 }
