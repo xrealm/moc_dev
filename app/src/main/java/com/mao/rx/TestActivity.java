756882e25f7264960dd19cb0dd12c572fd917d33
xrealm <mpging.12315@gmail.com>
2017-01-01 23:21:38 +0800

    zip

--- app/src/main/java/com/mao/rx/TestActivity.java
+++ app/src/main/java/com/mao/rx/TestActivity.java
@@ -1,11 +1,14 @@
 package com.mao.rx;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mao.dev.R;
 import com.orhanobut.logger.Logger;
+
+import java.util.ArrayList;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -13,6 +16,8 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -26,7 +31,87 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_test);
 
-        test();
+//        test();
+        testZip();
+    }
+
+    private void testZip() {
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                SystemClock.sleep(1000);
+                Logger.d("observable1 subscribe");
+                e.onNext(111);
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable<Integer> observable2 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                SystemClock.sleep(1000);
+                Logger.d("observable2 subscribe");
+                e.onNext(222);
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable<Integer> observable3 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                SystemClock.sleep(1000);
+                Logger.d("observable3 subscribe");
+                e.onNext(333);
+            }
+        }).subscribeOn(Schedulers.io());
+
+        ArrayList<Observable<Integer>> observables = new ArrayList<>();
+        observables.add(observable1);
+        observables.add(observable2);
+        observables.add(observable3);
+
+        Observable.zip(observables, new Function<Object[], String>() {
+            @Override
+            public String apply(Object[] integers) throws Exception {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Object integer : integers) {
+                    stringBuilder.append(integer).append(":");
+                }
+                return stringBuilder.toString();
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Logger.d("accept " + s);
+                    }
+                });
+//        Observable.zip(observable1, observable2, observable3, new Function3<Integer, Integer, Integer, String>() {
+//            @Override
+//            public String apply(Integer integer, Integer integer2, Integer integer3) throws Exception {
+//                Logger.d("apply");
+//                return integer + ":" + integer2 + ":" + integer3;
+//            }
+//        }).observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<String>() {
+//                    @Override
+//                    public void accept(String s) throws Exception {
+//                        Logger.d("accept " + s);
+//                    }
+//                });
+//        Observable.zip(observable1, observable2, new BiFunction<Integer, Integer, String>() {
+//            @Override
+//            public String apply(Integer integer, Integer integer2) throws Exception {
+//                SystemClock.sleep(1000);
+//                Logger.d("apply");
+//                return integer + ":" + integer2;
+//            }
+//        }).observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<String>() {
+//                    @Override
+//                    public void accept(String s) throws Exception {
+//                        Logger.d("accept " + s);
+//                    }
+//                });
+
     }
 
     private void test() {
@@ -45,34 +130,34 @@ public class TestActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<String>() {
 
-            private Disposable mDisposable;
+                    private Disposable mDisposable;
 
-            @Override
-            public void onSubscribe(Disposable d) {
-                Logger.d("onSubscribe");
-                mDisposable = d;
-            }
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Logger.d("onSubscribe");
+                        mDisposable = d;
+                    }
 
-            @Override
-            public void onNext(String value) {
-                Logger.d("onNext " + value);
-                if ("11".equals(value)) {
-                    Logger.d("dispose");
-                    mDisposable.dispose();
-                    Logger.d("isDisposed=" + mDisposable.isDisposed());
-                }
-            }
+                    @Override
+                    public void onNext(String value) {
+                        Logger.d("onNext " + value);
+                        if ("11".equals(value)) {
+                            Logger.d("dispose");
+                            mDisposable.dispose();
+                            Logger.d("isDisposed=" + mDisposable.isDisposed());
+                        }
+                    }
 
-            @Override
-            public void onError(Throwable e) {
+                    @Override
+                    public void onError(Throwable e) {
 
-            }
+                    }
 
-            @Override
-            public void onComplete() {
-                Logger.d("onComplete");
-            }
-        });
+                    @Override
+                    public void onComplete() {
+                        Logger.d("onComplete");
+                    }
+                });
     }
 
     private Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
