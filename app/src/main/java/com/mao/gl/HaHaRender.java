3006892704d3ab976e47e6f5e053724df213f1a4
xrealm <mpging.12315@gmail.com>
2017-05-11 08:24:00 +0800

    simple triangle

--- app/src/main/java/com/mao/gl/HaHaRender.java
+++ app/src/main/java/com/mao/gl/HaHaRender.java
@@ -23,6 +23,8 @@ public class HaHaRender implements GLSurfaceView.Renderer {
     private static final int BYTES_PER_FLOAT = 4;
 
     private int mProgram;
+    private int mPositionLocation;
+    private int mColorLocation;
     private Context mContext;
 
     float[] vCoords = {
@@ -51,16 +53,22 @@ public class HaHaRender implements GLSurfaceView.Renderer {
                 OpenGLUtil.readShaderFromRawResouce(mContext, R.raw.vertex_shader),
                 OpenGLUtil.readShaderFromRawResouce(mContext, R.raw.fragment_shader));
 
-
+        mPositionLocation = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(mPositionLocation);
+        GLES20.glVertexAttribPointer(mPositionLocation, 3, GLES20.GL_FLOAT, false, 0, floatBuffer);
+        mColorLocation = GLES20.glGetUniformLocation(mProgram, "vColor");
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0, 0, 600, 600);
+        GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUseProgram(mProgram);
+        GLES20.glUniform4fv(mColorLocation, 1, vColor, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 9);
     }
 }
